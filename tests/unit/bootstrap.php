21d051505c3090ca859e16d3c2add06a3f949a30
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-21 11:17:52 +0200

    test: provide ephemeral encryption secret to unit tests

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -6,3 +6,8 @@ if (!function_exists('dol_syslog')) {
         // no-op in tests
     }
 }
+
+// Unit tests use an ephemeral process-local secret; production secrets remain environment-only.
+if (getenv('BANKCONNECT_KEY_ENCRYPTION_SECRET') === false) {
+    putenv('BANKCONNECT_KEY_ENCRYPTION_SECRET='.bin2hex(random_bytes(32)));
+}
