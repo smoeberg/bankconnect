37d151ca631b7616489dfc0ce07485f0cd2344fc
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-21 10:05:48 +0200

    test: cover verified response decryption

--- tests/unit/BankConnectResponseSecurityTest.php
+++ tests/unit/BankConnectResponseSecurityTest.php
@@ -11,6 +11,7 @@ final class BankConnectResponseSecurityTest extends TestCase
     {
         $conf = new Conf();
         $conf->global['BANKCONNECT_BANK_CERTIFICATE'] = $this->cert;
+        $conf->global['BANKCONNECT_CUSTOMER_PRIVATE_KEY'] = $this->private;
         return new BankConnectResponseSecurity($conf);
     }
 
@@ -94,6 +95,111 @@ final class BankConnectResponseSecurityTest extends TestCase
         return $doc->saveXML();
     }
 
+    private function encryptedResponse(bool $tamper = false): string
+    {
+        $doc = new DOMDocument('1.0', 'UTF-8');
+        $doc->preserveWhiteSpace = false;
+        $doc->loadXML($this->response(), LIBXML_NONET | LIBXML_NOBLANKS | LIBXML_NOCDATA);
+        $xp = new DOMXPath($doc);
+        $xp->registerNamespace('s', 'http://schemas.xmlsoap.org/soap/envelope/');
+        $xp->registerNamespace('wsse', 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd');
+        $xp->registerNamespace('ds', 'http://www.w3.org/2000/09/xmldsig#');
+        $body = $xp->query('/s:Envelope/s:Body')->item(0);
+        $security = $xp->query('/s:Envelope/s:Header/wsse:Security')->item(0);
+        $this->assertInstanceOf(DOMElement::class, $body);
+        $this->assertInstanceOf(DOMElement::class, $security);
+
+        $payload = $doc->saveXML($body->firstChild);
+        $aesKey = random_bytes(32);
+        $iv = random_bytes(16);
+        $ciphertext = openssl_encrypt($payload, 'aes-256-cbc', $aesKey, OPENSSL_RAW_DATA, $iv);
+        $this->assertNotFalse($ciphertext);
+
+        $public = openssl_pkey_get_public($this->cert);
+        $this->assertNotFalse($public);
+        $wrapped = '';
+        $this->assertTrue(openssl_public_encrypt($aesKey, $wrapped, $public, OPENSSL_PKCS1_OAEP_PADDING));
+
+        while ($body->firstChild) {
+            $body->removeChild($body->firstChild);
+        }
+        $xenc = 'http://www.w3.org/2001/04/xmlenc#';
+        $ds = 'http://www.w3.org/2000/09/xmldsig#';
+        $ek = $doc->createElementNS($xenc, 'xenc:EncryptedKey');
+        $ek->setAttribute('Id', 'EK-test');
+        $method = $doc->createElementNS($xenc, 'xenc:EncryptionMethod');
+        $method->setAttribute('Algorithm', 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p');
+        $ek->appendChild($method);
+        $ki = $doc->createElementNS($ds, 'ds:KeyInfo');
+        $str = $doc->createElementNS('http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd', 'wsse:SecurityTokenReference');
+        $ref = $doc->createElementNS('http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd', 'wsse:Reference');
+        $ref->setAttribute('URI', '#EK-test');
+        $str->appendChild($ref);
+        $ki->appendChild($str);
+        $ek->appendChild($ki);
+        $cd = $doc->createElementNS($xenc, 'xenc:CipherData');
+        $cd->appendChild($doc->createElementNS($xenc, 'xenc:CipherValue', base64_encode($wrapped)));
+        $ek->appendChild($cd);
+        $rl = $doc->createElementNS($xenc, 'xenc:ReferenceList');
+        $dr = $doc->createElementNS($xenc, 'xenc:DataReference');
+        $dr->setAttribute('URI', '#ED-test');
+        $rl->appendChild($dr);
+        $ek->appendChild($rl);
+        $security->insertBefore($ek, $security->firstChild);
+
+        $ed = $doc->createElementNS($xenc, 'xenc:EncryptedData');
+        $ed->setAttribute('Id', 'ED-test');
+        $ed->setAttribute('Type', $xenc . 'Content');
+        $dm = $doc->createElementNS($xenc, 'xenc:EncryptionMethod');
+        $dm->setAttribute('Algorithm', 'http://www.w3.org/2001/04/xmlenc#aes256-cbc');
+        $ed->appendChild($dm);
+        $dki = $doc->createElementNS($ds, 'ds:KeyInfo');
+        $dstr = $doc->createElementNS('http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd', 'wsse:SecurityTokenReference');
+        $dref = $doc->createElementNS('http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd', 'wsse:Reference');
+        $dref->setAttribute('URI', '#EK-test');
+        $dstr->appendChild($dref);
+        $dki->appendChild($dstr);
+        $ed->appendChild($dki);
+        $ecd = $doc->createElementNS($xenc, 'xenc:CipherData');
+        $ecd->appendChild($doc->createElementNS($xenc, 'xenc:CipherValue', base64_encode($iv . $ciphertext)));
+        $ed->appendChild($ecd);
+        $body->appendChild($ed);
+
+        $signature = $xp->query('./ds:Signature', $security)->item(0);
+        $this->assertInstanceOf(DOMElement::class, $signature);
+        while ($signature->firstChild) {
+            $signature->removeChild($signature->firstChild);
+        }
+        $signedInfo = $doc->createElementNS($ds, 'ds:SignedInfo');
+        $cm = $doc->createElementNS($ds, 'ds:CanonicalizationMethod');
+        $cm->setAttribute('Algorithm', 'http://www.w3.org/2001/10/xml-exc-c14n#');
+        $signedInfo->appendChild($cm);
+        $sm = $doc->createElementNS($ds, 'ds:SignatureMethod');
+        $sm->setAttribute('Algorithm', 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256');
+        $signedInfo->appendChild($sm);
+        $sref = $doc->createElementNS($ds, 'ds:Reference');
+        $sref->setAttribute('URI', '#Id-body');
+        $transforms = $doc->createElementNS($ds, 'ds:Transforms');
+        $transform = $doc->createElementNS($ds, 'ds:Transform');
+        $transform->setAttribute('Algorithm', 'http://www.w3.org/2001/10/xml-exc-c14n#');
+        $transforms->appendChild($transform);
+        $sref->appendChild($transforms);
+        $digest = $doc->createElementNS($ds, 'ds:DigestMethod');
+        $digest->setAttribute('Algorithm', 'http://www.w3.org/2001/04/xmlenc#sha256');
+        $sref->appendChild($digest);
+        $digestBytes = hash('sha256', $body->C14N(true, false), true);
+        if ($tamper) {
+            $digestBytes = hash('sha256', 'tampered', true);
+        }
+        $sref->appendChild($doc->createElementNS($ds, 'ds:DigestValue', base64_encode($digestBytes)));
+        $signedInfo->appendChild($sref);
+        $signature->appendChild($signedInfo);
+        $signatureBytes = '';
+        $this->assertTrue(openssl_sign($signedInfo->C14N(true, false), $signatureBytes, $this->private, OPENSSL_ALGO_SHA256));
+        $signature->appendChild($doc->createElementNS($ds, 'ds:SignatureValue', base64_encode($signatureBytes)));
+        return $doc->saveXML();
+    }
+
     public function testValidResponseIsAccepted(): void
     {
         $verified = $this->security()->verify($this->response(),'getStatusResponse');
@@ -104,6 +210,39 @@ final class BankConnectResponseSecurityTest extends TestCase
         $nodes = $doc->getElementsByTagNameNS('http://bankconnect.dk/schema/2014','getStatusResponse');
         $this->assertSame(1, $nodes->length);
         $this->assertSame('getStatusResponse', $nodes->item(0)->localName);
+    }
+
+    public function testVerifiedEncryptedResponseIsDecryptedAndValidated(): void
+    {
+        $encrypted = $this->encryptedResponse();
+        $verified = $this->security()->verify($encrypted);
+        $decrypted = $this->security()->decrypt($verified, 'getStatusResponse');
+        $doc = new DOMDocument();
+        $this->assertTrue($doc->loadXML($decrypted, LIBXML_NONET | LIBXML_NOBLANKS | LIBXML_NOCDATA));
+        $nodes = $doc->getElementsByTagNameNS('http://bankconnect.dk/schema/2014', 'getStatusResponse');
+        $this->assertSame(1, $nodes->length);
+        $this->assertSame(0, $doc->getElementsByTagNameNS('http://www.w3.org/2001/04/xmlenc#', 'EncryptedData')->length);
+    }
+
+    public function testResponseTamperingFailsBeforeDecryption(): void
+    {
+        $this->expectException(BankConnectException::class);
+        $this->security()->verify($this->encryptedResponse(true));
+    }
+
+    public function testWrongCustomerKeyFailsClosed(): void
+    {
+        [, $otherCert] = $this->certificate();
+        $verified = $this->security()->verify($this->encryptedResponse());
+        $conf = new Conf();
+        $conf->global['BANKCONNECT_BANK_CERTIFICATE'] = $this->cert;
+        $otherKey = openssl_pkey_new(['private_key_bits'=>2048,'private_key_type'=>OPENSSL_KEYTYPE_RSA]);
+        $this->assertNotFalse($otherKey);
+        $otherPrivate = '';
+        $this->assertTrue(openssl_pkey_export($otherKey, $otherPrivate));
+        $conf->global['BANKCONNECT_CUSTOMER_PRIVATE_KEY'] = $otherPrivate;
+        $this->expectException(BankConnectException::class);
+        (new BankConnectResponseSecurity($conf))->decrypt($verified, 'getStatusResponse');
     }
 
     public function testTamperedBodyFailsClosed(): void
