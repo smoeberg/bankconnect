c0a6c1e154368ffa62329331c75e220abe55a9fa
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-21 11:20:37 +0200

    test: align certificate assertions with current security contract

--- tests/unit/CertificateManagerTest.php
+++ tests/unit/CertificateManagerTest.php
@@ -108,7 +108,7 @@ class CertificateManagerTest extends TestCase
         $mgr = new BankConnectCertificateManager($this->conf, $client, null, $store);
 
         $this->expectException(BankConnectException::class);
-        $this->expectExceptionMessage('does not match generated private key');
+        $this->expectExceptionMessage('No returned customer certificate matches the generated private key');
         try {
             $mgr->renewCustomerCertificateForAgreement($aid, '<serviceHeader/>');
         } finally {
@@ -366,7 +366,7 @@ class CertificateManagerTest extends TestCase
         $this->assertNotFalse($cert);
         openssl_x509_export($cert, $pem);
         $fp = $mgr->certificateFingerprint($pem);
-        $this->assertMatchesRegularExpression('/^[0-9A-F]{64}$/', $fp);
+        $this->assertMatchesRegularExpression('/^[0-9a-f]{64}$/', $fp);
         $this->assertSame($fp, $mgr->certificateFingerprint($pem));
     }
 
@@ -411,7 +411,7 @@ class CertificateManagerTest extends TestCase
         $this->assertNotFalse($cert2);
         openssl_x509_export($cert2, $pem2);
 
-        $this->assertSame($pem, $mgr->extractCustomerCertificatePem($pem2.$pem, $private));
+        $this->assertSame(rtrim($pem, "\r\n"), rtrim($mgr->extractCustomerCertificatePem($pem2.$pem, $private), "\r\n"));
     }
 }
 
