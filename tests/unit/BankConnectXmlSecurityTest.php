e876d6dcef2215709c857346d66f8e88e17d1e89
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-21 12:10:32 +0200

    test: lock v3.7 encryption and canonicalization profile

--- tests/unit/BankConnectXmlSecurityTest.php
+++ tests/unit/BankConnectXmlSecurityTest.php
@@ -165,6 +165,42 @@ class BankConnectXmlSecurityTest extends TestCase
         $this->assertSame(0,$xp->query('/s:Envelope/s:Header/ds:Signature')->length,'Signature must be inside wsse:Security');
     }
 
+    public function testXmlEncryptionSupportsV37Rsa15KeyTransport(): void
+    {
+        $soap='<?xml version="1.0"?>'
+            .'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:bc="http://bankconnect.dk/schema/2014">'
+            .'<soapenv:Header><bc:serviceHeader/><bc:technicalAddress/></soapenv:Header>'
+            .'<soapenv:Body><bc:transferPayment/></soapenv:Body>'
+            .'</soapenv:Envelope>';
+        $encrypted=$this->security()->encryptSoapBody($soap, BankConnectXmlSecurity::RSA_1_5);
+        $doc=new DOMDocument();
+        $this->assertTrue($doc->loadXML($encrypted,LIBXML_NONET));
+        $xp=new DOMXPath($doc);
+        $xp->registerNamespace('s','http://schemas.xmlsoap.org/soap/envelope/');
+        $xp->registerNamespace('xenc',BankConnectXmlSecurity::XENC_NS);
+        $this->assertSame(BankConnectXmlSecurity::RSA_1_5, $xp->query('/s:Envelope/s:Header/*[local-name()="Security"]/xenc:EncryptedKey/xenc:EncryptionMethod')->item(0)->getAttribute('Algorithm'));
+    }
+
+    public function testSignaturesDeclareInclusiveNamespacesForV37Canonicalization(): void
+    {
+        $soap='<?xml version="1.0"?>'
+            .'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:bc="http://bankconnect.dk/schema/2014">'
+            .'<soapenv:Header><bc:serviceHeader><bc:x>1</bc:x></bc:serviceHeader><bc:technicalAddress/></soapenv:Header>'
+            .'<soapenv:Body><bc:getStatus/></soapenv:Body>'
+            .'</soapenv:Envelope>';
+        $signed=$this->security()->signRequest($soap);
+        $doc=new DOMDocument();
+        $this->assertTrue($doc->loadXML($signed,LIBXML_NONET));
+        $xp=new DOMXPath($doc);
+        $xp->registerNamespace('s','http://schemas.xmlsoap.org/soap/envelope/');
+        $xp->registerNamespace('ds',BankConnectXmlSecurity::DS_NS);
+        $xp->registerNamespace('ec',BankConnectXmlSecurity::EXC_C14N);
+        $signature=$xp->query('/s:Envelope/s:Header/*[local-name()="Security"]/ds:Signature')->item(0);
+        $this->assertInstanceOf(DOMElement::class,$signature);
+        $this->assertSame('soapenv',$xp->query('./ds:SignedInfo/ds:CanonicalizationMethod/ec:InclusiveNamespaces',$signature)->item(0)->getAttribute('PrefixList'));
+        $this->assertSame('soapenv',$xp->query('./ds:SignedInfo/ds:Reference/ds:Transforms/ds:Transform/ec:InclusiveNamespaces',$signature)->item(0)->getAttribute('PrefixList'));
+    }
+
     public function testFailsClosedWithoutRequiredCertificates(): void
     {
         $empty=new BankConnectXmlSecurity(new Conf());
